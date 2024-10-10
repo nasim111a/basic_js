f56f5dd62f1e643afd15efe0fb5b06cc98dc33ee
nasim111a <nasim4638mm@gmail.com>
2024-10-10 05:52:08 +0000

    comparison

---
+++ .devcontainer/js_basic/comparison.js
@@ -0,0 +1,21 @@
+console.log(1>2);
+console.log(1>=2);
+console.log(1<2);
+console.log(1<=2);
+console.log(1!=2);
+console.log(1==2);
+console.log("********* 1 ********");
+
+console.log("1"<4);   // is trah ke coparison ko avoid karna h
+console.log(1<"4");    
+console.log("1"=="4");
+console.log("******** 2 *********");
+console.log(null>0);
+console.log(null>=0);
+console.log(null==0);
+console.log("********* 3 ********");
+console.log(undefined>0);
+console.log(undefined==0);
+console.log(undefined<0);
+
+
