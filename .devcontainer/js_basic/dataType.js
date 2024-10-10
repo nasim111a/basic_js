f7d2707529f64c5c1859b2f4a54386ae29f42eae
nasim111a <nasim4638mm@gmail.com>
2024-10-10 05:10:43 +0000

    datatupe

---
+++ .devcontainer/js_basic/dataType.js
@@ -0,0 +1,23 @@
+let number=12 //number
+let string = "nasim" // string
+let boolean = true // boolean
+let Undefined //undefined
+let Null= null // null 
+let Symbol 
+let object= {firstName:"nasim", lastName:"ahmad"} // object
+const cars = ["Saab", "Volvo", "BMW"];  // Array object:
+const date = new Date("2022-03-25");  // Date object:
+console.log(number);
+console.log(typeof number);
+console.log(string);
+console.log(typeof string);
+console.log(boolean);
+console.log(typeof boolean);
+console.log(Undefined);
+console.log(typeof Undefined);
+console.log(Null);
+console.log(typeof Null);
+console.log(object);
+console.log(typeof object);
+
+
