49d1bd189efbdf78755f9338c6efcb4285c58b9d
nasim111a <nasim4638mm@gmail.com>
2024-10-10 05:35:06 +0000

    operation

---
+++ .devcontainer/js_basic/operation.js
@@ -0,0 +1,19 @@
+console.log(2+3);
+console.log(2-3);
+console.log(2*3);
+console.log(2**3);
+console.log(2/3);
+console.log(2%3);
+let str1 = "hello"
+let str2 = " nasim"
+let str3 = str1+str2
+console.log(str3);
+
+console.log(1+"2");
+console.log("1"+2);
+console.log("1"+2+3);
+console.log(1+2+"3");
+
+
+
+
